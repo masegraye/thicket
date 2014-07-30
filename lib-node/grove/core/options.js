df67b3b4e977b406543944d1b69ea6b34cc38d65
Mason Browne <mason.browne@nebula.com>
2014-07-29 21:04:20 -0700

    Docs and units for Options.

--- lib-node/grove/core/options.js
+++ lib-node/grove/core/options.js
@@ -3,6 +3,31 @@
 var factory = function(
   _
 ) {
+
+  /**
+   *
+   * Helper for dealing with hash-style function arguments, which can be useful
+   * in methods with optional/default values.
+   *
+   * Usage:
+   *
+   *   var foo = function(opts) {
+   *       opts = Options.fromObject(opts);
+   *
+   *       var one = opts.getOrError("one"),
+   *           two = opts.getOrElse("two", 2),
+   *           three = opts.getOrElseFn("three", function() {
+   *             return 3;
+   *           });
+   *
+   *     console.log(one, two, three);
+   *   };
+   *
+   *   foo({ one: 1 }); // logs "1, 2, 3"
+   *   foo(); // throws Error
+   *   foo({one: 1, two: false, three: 4}); // logs "1, false, 4"
+   *
+   */
   var Options = function(opts) {
     this.initialize.apply(this, arguments);
   }
@@ -12,6 +37,15 @@ var factory = function(
       this._providedOpts = opts || {};
     },
 
+    /**
+     * Attempts to return the value for the requested `key`. If the value
+     * is undefined or null, instead returns the provided `fallbackValue`.
+     *
+     * @param key (required) attribute to be read
+     * @param fallbackValue  returned if `key` attribute is null or undefined
+     *
+     * @returns requested value for `key`, or fallbackValue`
+     */
     getOrElse: function(key, defaultValue) {
       if (this._isUndefOrNull(key)) {
         return defaultValue;
@@ -20,6 +54,16 @@ var factory = function(
       }
     },
 
+    /**
+     * Attempts to return the value for the requested `key`. If the value
+     * is undefined or null, throws an error.
+     *
+     * @param key (required) attribute to be read
+     * @param err            message provided to Error constructor if `key`
+     *                       is null or not defined
+     *
+     * @returns the value for the requested key
+     */
     getOrError: function(key, errorMsg) {
       if (this._isUndefOrNull(key)) {
         errorMsg = errorMsg || "Option requested but not found: `"+key+"`";
@@ -29,6 +73,20 @@ var factory = function(
       return this._providedOpts[key];
     },
 
+    /**
+     * Attempts to return the value for the requested `key`. If the value
+     * is undefined or null, instead evaluates `fallbackValueFn` and returns
+     * its value.
+     *
+     * This is useful for simulating non-strict evaluation.
+     *
+     * @param key (required)  attribute to be read
+     * @param fallbackValueFn evaluated and return value returned if `key`
+     *                        attribute is null or undefined
+     *
+     * @returns requested value for `key` or the return value of evaluated
+     *          `fallbackValueFn`
+     */
     getOrElseFn: function(key, defaultValueFn) {
       if (typeof defaultValueFn !== "function") {
         throw new Error("Options#getOrElseFn requires a defaultValueFn");
@@ -42,7 +100,7 @@ var factory = function(
     },
 
     _isUndefOrNull: function(key) {
-      return typeof this._providedOpts[key] === "undefined" || this._providedOpts === null;
+      return typeof this._providedOpts[key] === "undefined" || this._providedOpts[key] === null;
     }
   });
 
--- test/common/grove/core/test-options.js
+++ test/common/grove/core/test-options.js
@@ -7,5 +7,86 @@ var assert   = require("assert"),
 describe("Options", function() {
   it("should be defined", function() {
     assert.ok(Options);
-  })
+  });
+
+  it("should allow instantiation", function() {
+      var opts = new Options({
+          one: 1,
+          two: null,
+          three: 3,
+          four: undefined,
+          five: false
+      });
+      assert.ok(opts);
+  });
+
+  it("should allow factory construction", function() {
+      var opts = Options.fromObject({
+          one: 1,
+          two: null,
+          three: 3,
+          four: undefined,
+          five: false
+      });
+      assert.ok(opts);
+  });
+
+  describe("getOrElse", function() {
+      it("should use provided values on non-null and non-undefined keys, default otherwise", function() {
+          var opts = Options.fromObject({
+              one: 1,
+              two: null,
+              three: 3,
+              four: undefined,
+              five: false
+          });
+
+          assert.equal(opts.getOrElse("one", -1), 1);
+          assert.equal(opts.getOrElse("two", -1), -1);
+          assert.equal(opts.getOrElse("three", -1), 3);
+          assert.equal(opts.getOrElse("four", -1), -1);
+          assert.equal(opts.getOrElse("five", -1), false);
+      });
+  });
+
+  describe("getOrError", function() {
+      it("should use provided values on non-null and non-undefined keys, throw otherwise", function() {
+          var opts = Options.fromObject({
+              one: 1,
+              two: null,
+              three: 3,
+              four: undefined,
+              five: false
+          });
+
+          assert.equal(opts.getOrError("one"), 1);
+          assert.throws(function() {
+              opts.getOrError("two");
+          });
+          assert.equal(opts.getOrError("three"), 3);
+          assert.throws(function() {
+              opts.getOrError("four");
+          })
+          assert.equal(opts.getOrError("five"), false);
+      });
+  });
+
+  describe("getOrElseFn", function() {
+      it("should complain when called without defaultValueFn", function() {
+          var opts = Options.fromObject({
+              one: 1
+          });
+          assert.throws(function() {
+              opts.getOrElseFn("one");
+          });
+      });
+
+      it("should use evaluated defaultValueFn value as default, if provided key isn't present", function() {
+          var opts = Options.fromObject({
+              one: 1
+          });
+          assert.equal(opts.getOrElseFn("one", function() { return -1 }), 1);
+          assert.equal(opts.getOrElseFn("two", function() { return -1 }), -1);
+      })
+  });
 });
