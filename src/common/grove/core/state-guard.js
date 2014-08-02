95bddfd4af0cf614e0e9689b28b7f426953339bc
Mason Browne <mason.browne@nebula.com>
2014-08-01 17:27:41 -0700

    WIP: StateGuard stubs.

---
+++ src/common/grove/core/state-guard.js
@@ -0,0 +1,157 @@
+"use strict";
+
+var mod = function(
+  _,
+  Promise,
+) {
+
+  var StateGuard = function() {
+    this.initialize.apply(this, arguments);
+  };
+
+  _.extend(StateGuard.prototype, {
+    initialize: function(states) {
+      this._validStates = _.reduce(states, function(memo, state) {
+        memo[state] = true;
+        return memo;
+      }, {});
+
+      this._appliedStates = {};
+    },
+
+    apply: function() {
+      var states = aryOrVariadicToArray(arguments);
+      checkValidStatesOrThrow(this._validStates, states);
+
+      _.each(states, function(state) {
+        this._appliedStates[state] = true;
+      }, this);
+
+      return this;
+    },
+
+    applyAsync: Promise.method(function() {
+      return this.apply.apply(this, arguments);
+    }),
+
+    applyAsyncFn: function() {
+      var args = arguments;
+      return _.bind(function() {
+        return this.applyAsync.apply(this, args);
+      }, this);
+    },
+
+
+    unapply: function() {
+      var states = aryOrVariadicToArray(arguments);
+      checkValidStatesOrThrow(this._validStates, states);
+
+      _.each(states, function(state) {
+        if (this._appliedStates[state]) {
+          delete this._appliedStates[state];
+        }
+      }, this);
+
+      return this;
+    },
+
+    unapplyAsync: Promise.method(function() {
+      return this.unapply.apply(this, arguments);
+    }),
+
+    unapplyAsyncFn: function(states) {
+      var args = arguments;
+      return _.bind(function() {
+        return this.unapplyAsync.apply(this, args)
+      }, this);
+    },
+
+
+    deny: function() {
+      var states = aryOrVariadicToArray(arguments);
+      checkValidStatesOrThrow(this._validStates, states);
+      var denied = _.reject(states, function(state) {
+        return !this._appliedStates[state];
+      }, this);
+      if (denied.length > 0) {
+        throw new Error("State denial requested, but applied: " + pretty(state));
+      }
+
+      return this;
+    },
+
+    denyAsync: Promise.method(function() {
+      return this.deny.apply(this, arguments);
+    }),
+
+    denyAsyncFn: function() {
+      var args = arguments;
+      return _.bind(function() {
+        return this.denyAsync.apply(this, args)
+      }, this);
+    }
+
+
+    ensure: function() {
+      var states = aryOrVariadicToArray(arguments);
+      checkValidStatesOrThrow(this._validStates, states);
+    },
+
+    ensureAsync: Promise.method(function() {
+      return this.ensure.apply(this, arguments);
+    }),
+
+    ensureAsyncFn: function() {
+      var args = arguments;
+      return _.bind(function() {
+        return this.ensureAsync.apply(this, args);
+      }, this);
+    },
+
+
+    applied: function() {
+      var states = aryOrVariadicToArray(arguments);
+      checkValidStatesOrThrow(this._validStates, states);
+
+      return _.every(states, function(state) {
+        return this._appliedStates[state];
+      }, this);
+    },
+
+    appliedAsync: Promise.method(function() {
+      return this.applied.apply(this, arguments);
+    }),
+
+    appliedAsyncFn: function() {
+      var args = arguments;
+      return _.bind(function() {
+        return this.appliedAsync.apply(this, args)
+      }, this);
+    }
+  });
+
+  var aryOrVariadicToArray = function(args) {
+    return _.chain(args).toArray().flatten();
+  };
+
+  var checkValidStatesOrThrow = function(validStatesMap, states) {
+    var invalidStates = _.reject(states, function(state) {
+      return !!validStatesMap[state];
+    });
+
+    if (invalidStates.length > 0) {
+      throw new Error("Invalid states provided: " + pretty(invalidStates));
+    }
+  };
+
+  var pretty = function(states) {
+    return "["+states.join(",")+"]";
+  };
+
+  return StateGuard;
+};
+
+module.exports = mod(
+  require("underscore"),
+  require("bluebird")
+);
